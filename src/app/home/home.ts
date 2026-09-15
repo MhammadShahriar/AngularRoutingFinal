207714a892eb9bdb9aa3d47f5268f5a71bd6a50f
mrmdshahriar <shahriar.ict@gmail.com>
2026-09-15 17:45:45 +0600

    sdsdssd

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth';
 
 import { ActivatedRoute } from '@angular/router';
-
 @Component({
-
   imports: [ButtonModule, TableModule, CommonModule],
+  standalone: true,
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
+
 export class Home implements OnInit {
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   users:any[] = [];
+
+  logout(): void {
+
+    this.authService.logout();
+
+    this.router.navigate(['/login']);
+  }
 
   //constructor(private route: ActivatedRoute) {}
 
